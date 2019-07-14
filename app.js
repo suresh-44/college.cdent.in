8d56952896ebc99385137a7b50a93f364ed5a4ce
Pushkar Anand <anandpushkar088@gmail.com>
2019-07-15 03:29:49 +0530

    Lint fixes.

--- app.js
+++ app.js
@@ -29,17 +29,17 @@ app.use(cookieParser());
 
 // session setup
 const sessionOptions = {
-  store: new FileStore({}),
-  secret: process.env.SESSION_SECRET,
-  resave: true,
-  saveUninitialized: false,
-  cookie: {secure: false},
+	store: new FileStore({}),
+	secret: process.env.SESSION_SECRET,
+	resave: true,
+	saveUninitialized: false,
+	cookie: {secure: false},
 };
 
 
 if (process.env.NODE_ENV === "production") {
-  app.set("trust proxy", 1);
-  sessionOptions.cookie.secure = true; // for https connections
+	app.set("trust proxy", 1);
+	sessionOptions.cookie.secure = true; // for https connections
 }
 
 app.use(session(sessionOptions));
@@ -58,18 +58,18 @@ app.use("/super/admin", superAdminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+	next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+	// render the error page
+	res.status(err.status || 500);
+	res.render("error");
 });
 
 module.exports = app;
