9160082c68dcac5fe89aa9210a6cacb196d95f2d
Pushkar Anand <anandpushkar088@gmail.com>
2019-07-19 23:17:00 +0530

    fixed all styling issues.

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const FileStore = require("session-file-store")(session);
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
-const hbs = require('hbs')
-const methodOveride = require('method-override');
+const hbs = require("hbs");
+const methodOveride = require("method-override");
 
 require("dotenv").config();
 
@@ -19,17 +19,17 @@ const app = express();
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
-hbs.registerHelper('each', (items, options) => {
-	var out="";
-	for(var i=0, l=items.length; i<l; i++) {
-    out += options.fn(items[i]) ;
+hbs.registerHelper("each", (items, options) => {
+	let out = "";
+	for (let i = 0, l = items.length; i < l; i++) {
+		out += options.fn(items[i]);
 	}
 	return out;
-})
-hbs.registerPartials(__dirname+ '/views/partials')
+});
+hbs.registerPartials(__dirname + "/views/partials");
 
 // Method Override Middleware
-app.use(methodOveride('_method'))
+app.use(methodOveride("_method"));
 
 app.use(logger("dev"));
 app.use(express.json());
--- services/super.admin.js
+++ services/super.admin.js
@@ -4,61 +4,62 @@ const TempModel = require("../database/models/temp-model");
 const hash = crypto.createHash("sha512");
 
 const superAdminServices = {
-  login: async (req, res) => {
-    const inputPwdHash = crypto
-      .createHash("sha512")
-      .update(req.body.password, "utf8")
-      .digest("hex");
+	login: async (req, res) => {
+		const inputPwdHash = crypto
+			.createHash("sha512")
+			.update(req.body.password, "utf8")
+			.digest("hex");
 
-    if (process.env.SUPER_ADMIN_PWD === inputPwdHash) {
-      // Password is correct Login user now
+		if (process.env.SUPER_ADMIN_PWD === inputPwdHash) {
+			// Password is correct Login user now
 
-      const browser = req.headers["user-agent"];
-      const userIP =
-        req.header("x-forwarded-for") ||
-        req.connection.remoteAddress + inputPwdHash;
-      const str = browser + userIP;
-      const secret = crypto.createHash("sha512").update(str, "utf8");
-      req.session.login = true;
-      req.session.superAdmin = true;
-      req.session.user = req.body.username;
-      req.session.secret = secret.digest("hex");
-      res.redirect("admin/dashboard/");
-    } else {
-      throw new Error("Incorrect Password or username");
-    }
-  },
-  checkLogin: async (req, res) => {
-    const browser = req.headers["user-agent"];
-    const userIP =
-      req.header("x-forwarded-for") || req.connection.remoteAddress;
-    const str = browser + userIP + process.env.SUPER_ADMIN_PWD;
-    const loginHash = hash.update(str, "utf8").digest("hex");
-    if (
-      !(
-        req.session.login &&
-        req.session.superAdmin &&
-        loginHash === req.session.secret
-      )
-    ) {
-      res.redirect("/super/admin");
-    }
-  },
-  logout: (req, res) => {
-		req.session.login = false;
-      req.session.superAdmin = false;
-      req.session.user = null;
-			req.session.secret = null;
-			res.redirect("/");
+			const browser = req.headers["user-agent"];
+			const userIP =
+				req.header("x-forwarded-for") ||
+				req.connection.remoteAddress + inputPwdHash;
+			const str = browser + userIP;
+			const secret = crypto.createHash("sha512").update(str, "utf8");
+			req.session.login = true;
+			req.session.superAdmin = true;
+			req.session.user = req.body.username;
+			req.session.secret = secret.digest("hex");
+			res.redirect("admin/dashboard/");
+		} else {
+			throw new Error("Incorrect Password or username");
+		}
 	},
-  getCollegeData: async () => {
-    return TempModel.find();
+	checkLogin: async (req, res) => {
+		const browser = req.headers["user-agent"];
+		const userIP =
+			req.header("x-forwarded-for") || req.connection.remoteAddress;
+		const str = browser + userIP + process.env.SUPER_ADMIN_PWD;
+		const loginHash = hash.update(str, "utf8").digest("hex");
+		if (
+			!(
+				req.session.login &&
+				req.session.superAdmin &&
+				loginHash === req.session.secret
+			)
+		) {
+			res.redirect("/super/admin");
+		}
+	},
+	logout: (req, res) => {
+		req.session.login = false;
+		req.session.superAdmin = false;
+		req.session.user = null;
+		req.session.secret = null;
+		res.redirect("/");
+	},
+	getCollegeData: async () => {
+		return TempModel.find();
 	},
 	removeCollege: async (req, res) => {
 		const id = req.params.id;
-	  await TempModel.findByIdAndRemove(id)
+		await TempModel.findByIdAndRemove(id);
 	},
-  acceptCollege: async collegeID => {}
+	acceptCollege: async (collegeID) => {
+	},
 };
 
 module.exports = superAdminServices;
