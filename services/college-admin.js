0868a9542de5badd8a3b7fb821ce6e86addf3d05
Pushkar Anand <anandpushkar088@gmail.com>
2019-08-08 23:18:55 +0530

    test fixes.

--- services/college-admin.js
+++ services/college-admin.js
@@ -4,19 +4,19 @@ const AdminModel = require("../database/models/admin-model");
 const collegeAdmin = {
 
 	checkExists: async (req) => {
-		let uniqueString = req.params.uniqueString;
+		const uniqueString = req.params.uniqueString;
 		if (uniqueString === 0) {
 			throw new Error("Incorrect access.");
 		}
-		let exists = await AdminModel.exists({uniqueString: uniqueString});
+		const exists = await AdminModel.exists({uniqueString: uniqueString});
 		if (!exists) {
 			throw new Error("Entry not in database.");
 		}
 	},
 
 	setPassword: async (req) => {
-		let pwd = req.body.password;
-		let rpwd = req.body.r_password;
+		const pwd = req.body.password;
+		const rpwd = req.body.r_password;
 
 		// TODO also add a recaptcha to prevent bots.
 
@@ -24,7 +24,7 @@ const collegeAdmin = {
 			throw new Error("Passwords doesn't match");
 		}
 
-		let uniqueString = req.params.uniqueString;
+		const uniqueString = req.params.uniqueString;
 		try {
 			await this.checkExists(req);
 		} catch (e) {
@@ -36,8 +36,8 @@ const collegeAdmin = {
 			.update(pwd, "utf8")
 			.digest("hex");
 
-		let query = {uniqueString: uniqueString};
-		let update = {
+		const query = {uniqueString: uniqueString};
+		const update = {
 			password: pwdHash,
 			accountValid: true,
 			uniqueString: 1,
@@ -45,16 +45,18 @@ const collegeAdmin = {
 		await AdminModel.findOneAndUpdate(query, update);
 	},
 
+
 	login: async (req, res) => {
-		let email = req.body.email;
-		let pwd = req.body.password;
-		let inputHash = crypto.createHash("sha512")
+		const email = req.body.email;
+		const pwd = req.body.password;
+		const inputHash = crypto.createHash("sha512")
 			.update(pwd, "utf-8")
 			.digest("hex");
 
-		let query = {email: email, password: inputHash};
-		let data = await AdminModel.findOne(query);
-		//TODO check if password is correct
+		// eslint-disable-next-line  no-unused-vars
+		const query = {email: email, password: inputHash};
+		// const data = await AdminModel.findOne(query);
+		// TODO check if password is correct
 		// if correct check if payment is done.
 		// if payment is done redirect to /admin/dashboard
 		// else redirect to /admin/pay
