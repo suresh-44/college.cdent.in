9115955f278f09b7372ccb0f7993c085503b32f2
suresh-44 <sureshsunsuri@gmail.com>
2019-09-06 19:54:16 +0530

    removed admin schema methods

--- database/models/schemas/admin.js
+++ database/models/schemas/admin.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 const crypto = require("crypto");
-const jwt = require("jsonwebtoken");
 
 const collegeAdmin = new mongoose.Schema({
 	name: {
@@ -85,56 +84,6 @@ const createHash = (key) => {
 };
 
 /**
- * generate the jwt token for authentication
- * @return {String} token generated jwt Token is saved in database and return
- */
-collegeAdmin.methods.generateAuthToken = function() {
-	const admin = this;
-	const access = "auth";
-
-	// create the jwt token
-	const token = jwt.sign({
-		// expire time for the token
-		exp: ~~((Date.now() / 1000) + (60 * 60)), // valid only for 1hr
-		_id: admin._id.toHexString(),
-		access,
-	}, process.env.JWT_SECRET, (err, token) => {
-		if (!err && token) return token;
-		else throw new Error(err);
-	});
-
-	// pushing the token to the database
-	admin.tokens.push({access, token});
-
-	// saving token in the database and return the token
-	return admin.save().then(()=> {
-		return token;
-	});
-};
-
-/**
- * find the admin using provided token
- * @param {String} token
- * @return {Promise<admin>} admin
- */
-collegeAdmin.statics.findByToken = async function(token) {
-	const Admin = this;
-	let decode;
-
-	try {
-		decode = jwt.verify(token, process.env.JWT_SECRET);
-	} catch (e) {
-		return Promise.reject(e);
-	}
-
-	return await Admin.findOne({
-		"_id": decode.id,
-		"tokens.token": token,
-		"tokens.access": "auth",
-	});
-};
-
-/**
  *
  * @param {String} email to find the admin
  * @param {String} password for validation
@@ -155,22 +104,6 @@ collegeAdmin.statics.findByCredentials = async function(email, password) {
 	} else {
 		throw new Error("Password is incorrect");
 	}
-};
-
-/**
- * Remove the token from the database
- * @param {String} token
- * @return {String} removed token from the database
- */
-collegeAdmin.methods.removeToken = function(token) {
-	// remove the token from database
-	return this.update({
-		$pull: {
-			tokens: {
-				token,
-			},
-		},
-	});
 };
 
 // hash the password before saving to the database
